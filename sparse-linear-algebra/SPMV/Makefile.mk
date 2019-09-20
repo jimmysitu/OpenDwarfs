32ddd4d5286499b1dc4b65e6dd1b6c7e24dd2ab8
Jimmy Situ <web@jimmystone.cn>
2019-09-20 16:30:50 +0800

    Update link lib opt in csr

--- sparse-linear-algebra/SPMV/Makefile.mk
+++ sparse-linear-algebra/SPMV/Makefile.mk
@@ -8,12 +8,12 @@
 bin_PROGRAMS += csr
 bin_PROGRAMS += createcsr
 
-csr_LDFLAGS = -lm 
+csr_LDFLAGS = -lm
 csr_SOURCES = sparse-linear-algebra/SPMV/src/csr.c sparse-linear-algebra/SPMV/src-common/sparse_formats.c sparse-linear-algebra/SPMV/src-common/ziggurat.c sparse-linear-algebra/SPMV/src-common/common.c
 createcsr_SOURCES = sparse-linear-algebra/SPMV/src-test/createcsr.c sparse-linear-algebra/SPMV/src-common/sparse_formats.c sparse-linear-algebra/SPMV/src-common/ziggurat.c sparse-linear-algebra/SPMV/src-common/common.c
 
 ##createcsr does not need to be linked with any of the opencl common files
-createcsr_LDADD = include/common_args.o include/rdtsc.o opts/opts.o
+createcsr_LDADD = include/common_args.o include/rdtsc.o opts/opts.o -lm
 createcsr_LINK = $(CCLD) -o $@ -lm
 
 all_local += csr-all-local
